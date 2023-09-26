82a08845a107eedbd40bb6d7e8af274fae3820fc
Jeff Maley <jeffmaley@gmail.com>
2023-09-26 23:28:01 +0000

    added time

--- apprunner-check.py
+++ apprunner-check.py
@@ -1,8 +1,11 @@
 import requests
+import time
 
 def main():
-    h = requests.get("http://169.254.169.254/1.0/meta-data")
-    print(h.text)
+    while True:
+        h = requests.get("http://169.254.169.254/1.0/meta-data")
+        print(h.text)
+        time.sleep(60)
 
 
 if __name__ == "__main__":
